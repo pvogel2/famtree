0b413877d89dcef1d2227deaf46b5bf9f5581ab2
pvogel2 <pvogel2@freenet.de>
2022-08-25 10:15:53 +0200

    refactor: set relation colors to foreground

--- src/components/relations/PartnerRelation.js
+++ src/components/relations/PartnerRelation.js
@@ -1,10 +1,11 @@
 import { useEffect, useContext } from 'react';
-import { Vector3, Line, BufferGeometry, LineBasicMaterial } from 'three';
+import { useSelector } from 'react-redux';
+import { Vector3, Line, BufferGeometry, LineBasicMaterial, Color } from 'three';
 import RenderContext from './../RenderContext.js';
 
-function getRelationLines(s, t) {
+function getRelationLines(s, t, config = { color: 0xffffff }) {
   const material = new LineBasicMaterial({
-    color: 0x9999ff,
+    color: new Color(config.color),
   });
 
   const points = [];
@@ -17,17 +18,23 @@ function getRelationLines(s, t) {
   return new Line( geometry, material );
 }
 
+const getForeground = (state) => state.layout.foreground;
+
 function PartnerRelation(props) {
   const { source, target, parent } = props;
   const { renderer } = useContext(RenderContext);
+  const foreground = useSelector(getForeground);
 
   useEffect(() => {
     if (!renderer || !target || !parent) return;
     const s = source ? source.clone() : new Vector3();
 
-    const lines = getRelationLines(s, target);
+    const lines = getRelationLines(s, target, { color: foreground });
     parent.add(lines);
-  }, [renderer, source, target, parent]);
+    return () => {
+      parent.remove(lines);
+    };
+  }, [renderer, source, target, parent, foreground]);
 
   return null;
 };
