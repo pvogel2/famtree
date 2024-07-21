8b366ad094e86fe8e05c659cc80e5ef495998145
Peter Vogel <pvogel2@freenet.de>
2024-07-21 12:49:49 +0200

    chore: restruct gedcom

--- admin/js/gedcom/parser.js
+++ admin/js/gedcom/parser.js
@@ -1,23 +1,28 @@
-const TAGS = {
+export const TAGS = {
   HEAD: 'head',
   INDI: 'indi',
+  FAM: 'fam',
   GEDC: 'gedc',
   VERS: 'version',
   NAME: 'name',
   GIVN: 'givn',
   SURN: 'surn',
+  HUSB: 'husb',
+  WIFE: 'wife',
+  CHIL: 'chil',
+  BIRT: 'birth',
+  DEAD: 'death',
+  DATE: 'date',
 };
 
 const re = new RegExp(/^(?<level>^\d+)(\s(?<id>\@[a-zA-Z0-9]+\@))?\s(?<tag>[a-zA-Z]+)(\s(?<value>.*))?/);
-export default class GedcomParser {
-  constructor(raw = '') {
-    this.raw = raw;
-  }
 
-  parse() {
-    const tags = this.raw.split('\n');
+export default class GedcomParser {
+  parse(raw = '') {
+    const tags = raw.split('\n');
     const result = {
       individuals: [],
+      relations: [],
     };
     let current;
 
@@ -37,6 +42,9 @@ export default class GedcomParser {
           } else if (tag === TAGS.INDI) {
             current.id = id;
             result.individuals.push(current);
+          } else if (tag === TAGS.FAM) {
+            current.id = id;
+            result.relations.push(current);
           }
         }
 
